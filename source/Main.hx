a82c1c2b022e471c48f6321548d946b9a8e3d88d
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-27 06:54:48 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -3,7 +3,13 @@ package;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
+import debug.FPSCounter;
+import openfl.display.StageScaleMode;
 import openfl.Assets;
+import haxe.io.Path;
+#if android
+import android.content.Context;
+#end
 import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
@@ -18,19 +24,31 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:FPS;
+	public static var fpsVar:FPSCounter;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
 	public static function main():Void
 	{
 		Lib.current.addChild(new Main());
+		#if cpp
+		cpp.NativeGc.enable(true);
+		#elseif hl
+		hl.Gc.enable(true);
+		#end
 	}
 
 	public function new()
 	{
 		super();
 
+		// Credits to MAJigsaw77 (he's the og author for this code)
+		#if android
+		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
+		#elseif ios
+		Sys.setCwd(lime.system.System.applicationStorageDirectory);
+		#end
+		
 		if (stage != null)
 		{
 			init();
@@ -53,34 +71,21 @@ class Main extends Sprite
 
 	private function setupGame():Void
 	{
-		var stageWidth:Int = Lib.current.stage.stageWidth;
-		var stageHeight:Int = Lib.current.stage.stageHeight;
-
-		if (zoom == -1)
-		{
-			var ratioX:Float = stageWidth / gameWidth;
-			var ratioY:Float = stageHeight / gameHeight;
-			zoom = Math.min(ratioX, ratioY);
-			gameWidth = Math.ceil(stageWidth / zoom);
-			gameHeight = Math.ceil(stageHeight / zoom);
-		}
-
-		#if !debug
-		initialState = TitleState;
-		#end
-
 		Paths.getModFolders();
 		ClientPrefs.startControls();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
+	        fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
-		if(fpsVar != null) {
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		if(fpsVar != null)
 			fpsVar.visible = ClientPrefs.showFPS;
-		}
-		#end
 
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
+		#end
+		
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
