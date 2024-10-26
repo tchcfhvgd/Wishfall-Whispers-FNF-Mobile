8732ebec74f5a6a03a8d63bdacb7f1fe5cbc7b50
SylverStar82 <silverstrike82@gmail.com>
2024-10-25 23:48:48 -0700

    everything

---
+++ assets/preload/data/hearts-apart/dialogueEnd.lua
@@ -0,0 +1,19 @@
+local allowCountdown = false
+local startedEndDialogue = false
+
+function onEndSong()
+    if not allowCountdown and isStoryMode and not startedEndDialogue then
+        setProperty('inCutscene', true);
+        runTimer('startDialogueEnd', 0.8);
+        startedEndDialogue = true;
+        return Function_Stop;
+    end
+
+    return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+    if tag == 'startDialogueEnd' then
+        startDialogue('dialogueEnd', 'rainAmbience');
+    end
+end
