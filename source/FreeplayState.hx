82ef6db17a9e6c22ceef1c7a696d8ebad1d0bcd0
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-27 11:57:38 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -267,14 +267,8 @@ class FreeplayState extends MusicBeatState
 		add(lucidFlash);
 		FlxTween.color(lucidFlash, 1, 0x00000000, 0x00f1a9ff);
 		super.create();
-		var video:FlxVideo = new FlxVideo();
-		video.load(Paths.video('wipeIn'));
-		video.play();
-		video.onEndReached.add(function()
-		{
-		        video.dispose();
-			return;
-		}, true);
+		var wipe:CustomWipeTransition = new CustomWipeTransition();
+		wipe.startVideoWipe('wipeIn');
 	}
 
 	override function closeSubState() {
@@ -405,14 +399,9 @@ class FreeplayState extends MusicBeatState
 		}
 		else #end if (accepted)
 		{
-			var video:FlxVideo = new FlxVideo();
-		                                                        video.load(Paths.video('wipeOut'));
-		                                                        video.play();
-		                                                        video.onEndReached.add(function()
-		                                                        {
-			                                                     video.dispose();
-			                                                     return;
-		                                                        }, true);
+			var wipe:CustomWipeTransition = new CustomWipeTransition();
+			wipe.startVideoWipe('wipeOut');
+									
 			var songLowercase:String = Paths.formatToSongPath(songs[curSelected].songName);
 			if((curDifficulty == 3)) 
 				var songLowercase:String = Paths.formatToSongPath(songs[curSelected].songName);
