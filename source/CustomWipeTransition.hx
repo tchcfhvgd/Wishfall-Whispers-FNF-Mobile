3d2bba0a279897d782c8f139b23e2d7b73befe6f
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-10-27 11:03:18 +0800

    Update CustomWipeTransition.hx

--- source/CustomWipeTransition.hx
+++ source/CustomWipeTransition.hx
@@ -21,27 +21,10 @@ import sys.FileSystem;
 import CustomFadeTransition;
 
 class CustomWipeTransition extends MusicBeatSubstate {
-	public static var finishCallback:Void->Void;
 	var isTransIn:Bool = false;
-	static var leVideo:FlxVideo = null;
 
 	public function new() {
 		super();
-
-		// if(isTransIn) {
-		// 	startVideoWipe('wipeIn');
-		// } else {
-		// 	startVideoWipe('wipeOut');
-		// }
-
-
-		//this.isTransIn = isTransIn;
-
-		// if(isTransIn) {
-		// 	startVideoWipe('wipeIn');
-		// } else {
-		// 	startVideoWipe('wipeOut');
-		// }
 
 	}
 
@@ -50,76 +33,21 @@ class CustomWipeTransition extends MusicBeatSubstate {
 
 		if(!ClientPrefs.lowQuality)
 		{
-			#if VIDEOS_ALLOWED
-			var foundFile:Bool = false;
-			var fileName:String = #if MODS_ALLOWED Paths.modFolders('videos/' + name + '.' + Paths.VIDEO_EXT); #else ''; #end
-			#if sys
-			if(FileSystem.exists(fileName)) {
-				foundFile = true;
-			}
-			#end
-
-			if(!foundFile) {
-				
-				fileName = Paths.video(name);
-				#if sys
-				if(FileSystem.exists(fileName)) {
-				#else
-				if(OpenFlAssets.exists(fileName)) {
-				#end
-					foundFile = true;
-				}
-			}
-
-			if(foundFile) {
-
-
-				
-				//inCutscene = true;
-				// var bg2 = new FlxSprite(-FlxG.width, -FlxG.height).makeGraphic(FlxG.width * 3, FlxG.height * 3, FlxColor.BLACK);
-				// bg2.scrollFactor.set();
-				// bg2.cameras = [camGame];
-				// add(bg2);
-				
-				// if(name == 'wipeOut')
-				// 	{	
-				// 		new FlxTimer().start(0.1, function(tmr:FlxTimer)
-				// 	{
-				// 		var blackBarThingie:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, 56, FlxColor.BLACK);
-				// 		add(blackBarThingie);
-				// 	});
-				// 		return;
-				// 	}
-						#if desktop
-				if (leVideo != null) {
-					leVideo.skipVideo();
-				}
-				#end
-				(leVideo = new FlxVideo(fileName)).finishCallback = function() 
-				{
-					leVideo = null;
-					if(name == "wipeOut")
+			var video:FlxVideo = new FlxVideo();
+		        video.load(Paths.video(name));
+		        video.play();
+		        video.onEndReached.add(function()
+		        {
+			        video.dispose();
+				if(name == "wipeOut")
 						{
 							trace("BOOTIE BOY");
 							CustomFadeTransition.screenBlack();
 							
 						}
-				}
-				
-				return;
-			} else {
-				
-				FlxG.log.warn('Couldnt find video file: ' + fileName);
-			}
-			#end
-
-		}
-
-		// if(endingSong) {
-		// 	endSong();
-		// } else {
-		// 	startCountdown();
-		// }
+			        return;
+		        }, true);
+		
 	}
 
 	override function update(elapsed:Float) {
@@ -127,15 +55,5 @@ class CustomWipeTransition extends MusicBeatSubstate {
 	}
 
 	override function destroy() {
-		// if(leTween != null) {
-		// 	#if MODS_ALLOWED
-		// 	if(isTransIn) {
-		// 		Paths.destroyLoadedImages();
-		// 	}
-		// 	#end
-		// 	finishCallback();
-		// 	leTween.cancel();
-		// }
-		// super.destroy();
 	}
 }
